74e0310c364618acff8780fc496fe1432472c97e
rahul <rahul.battinarb@gmail.com>
2023-02-21 03:47:06 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import './App.css'
 
 function App() {
+  const [forms, setForms] = useState([{ message: "", response: "" }]);
+  const [count, setCount] = useState(1);
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = (e, index) => {
+    e.preventDefault();
+
+    setLoading(true);
+
+    const message = forms[index].message;
+    axios.post("https://combative-tan-bee.cyclic.app/", { message }).then((res) => {
+      console.log(res);
+      const newForms = [...forms];
+      newForms[index].response = res.data.choices[0].text;
+      setForms(newForms);
+      setLoading(false);
+      if (forms[index + 1] === undefined) {
+        setForms([...newForms, { message: "", response: "" }]);
+        setCount(count + 1);
+      }
+    });
+  };
+
+  const handleChange = (e, index) => {
+    const newForms = [...forms];
+    newForms[index].message = e.target.value;
+    setForms(newForms);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Flight details helper</h1>
+      {forms.map((form, index) => (
+        <form onSubmit={(e) => handleSubmit(e, index)} key={index}>
+          <div className="reqresdiv">
+            <div className="input-container">
+              <textarea
+                id={`input-${index}`}
+                placeholder="Type your query here"
+                value={form.message}
+                onChange={(e) => handleChange(e, index)}
+                disabled={form.response || loading}
+              ></textarea>
+            </div>
+
+            <div className="output-container">
+              {(
+                form.response && (
+                  <div>
+                    <b>{form.response}</b>
+                  </div>
+                )
+              )}
+            </div>
+          </div>
+
+          {form.response ? (
+            index === forms.length - 1 ? (
+              <button onClick={() => setForms([...forms, { message: "", response: "" }])} id="add-form">
+                Add Form
+              </button>
+            ) : null
+          ) : (
+            <button disabled={loading} id="submit">
+              {loading ? "Loading..." : "Submit"}
+            </button>
+          )}
+        </form>
+      ))}
     </div>
   );
 }
