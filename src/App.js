2d4cd431adc455dde430920009b8c22e9b81dc94
rahul <rahul.battinarb@gmail.com>
2023-02-22 16:07:07 +0530

    added text to speech feature

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React, { useState } from "react";
 import axios from "axios";
 import './App.css'
+import { useSpeechSynthesis } from "react-speech-kit";
+import { FaVolumeMute, FaVolumeUp } from "react-icons/fa";
 
 function App() {
   const [forms, setForms] = useState([{ message: "", response: "" }]);
   const [count, setCount] = useState(1);
   const [loading, setLoading] = useState(false);
+  const [mute, setMute] = useState(true); // initially muted
+  const { speak } = useSpeechSynthesis();
 
   const handleSubmit = (e, index) => {
     e.preventDefault();
-
+  
     setLoading(true);
-
+  
     const message = forms[index].message;
-    axios.post("https://combative-tan-bee.cyclic.app/", { message }).then((res) => {
+    axios.post("http://localhost:3002/", { message }).then((res) => {
       console.log(res);
       const newForms = [...forms];
       newForms[index].response = res.data.choices[0].text;
@@ -22,19 +26,29 @@ function App() {
       if (forms[index + 1] === undefined) {
         setForms([...newForms, { message: "", response: "" }]);
         setCount(count + 1);
+        if (!mute) {
+          speak({ text: newForms[index].response }); // speak the response
+        }
       }
     });
   };
-
+  
   const handleChange = (e, index) => {
     const newForms = [...forms];
     newForms[index].message = e.target.value;
     setForms(newForms);
   };
 
+  const toggleMute = () => {
+    setMute(!mute);
+  };
+
   return (
     <div className="container">
       <h1>Flight details helper</h1>
+      <div className="mute-icon-container" onClick={toggleMute}>
+        {mute ? <FaVolumeMute /> : <FaVolumeUp />}
+      </div>
       {forms.map((form, index) => (
         <form onSubmit={(e) => handleSubmit(e, index)} key={index}>
           <div className="reqresdiv">
